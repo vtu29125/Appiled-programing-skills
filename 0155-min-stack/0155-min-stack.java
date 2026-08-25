6740fe8114f7df01a194e7a90cf2c694d70dc5ea
CH Ganesh srinivas <vtu29125@veltech.edu.in>
2026-08-25 14:17:38 +0530

    Time: 34 ms (61.57%), Space: 99.8 MB (64.55%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
